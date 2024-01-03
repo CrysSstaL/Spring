0fcd657c4216894180da96bc181f06fd4b50186f
Vasiliy <Mst.Vasek999@yandex.ru>
2024-01-03 10:17:47 +0300

    sq-ch1-ex2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,25 @@
 package org.example;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextException;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext();
-        Parrot p = new Parrot();
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+
+        String stroka = context.getBean(String.class);
+        System.out.println(stroka);
+
+        Integer ten = context.getBean(Integer.class);
+        System.out.println(ten);
     }
 }
 
