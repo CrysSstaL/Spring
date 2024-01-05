efb32edcf372b7fd0f936302c82e5f42eabcded6
Vasiliy <Mst.Vasek999@yandex.ru>
2024-01-05 13:45:28 +0300

    sq-ch3-ex8 Выбор из нескольких бинов в контексте Spring

--- src/main/java/main/Parrot.java
+++ src/main/java/main/Parrot.java
@@ -7,12 +7,7 @@ import org.springframework.stereotype.Component;
 public class Parrot {
 
     private String name = "Koko";
-    private final Person person;
 
-    @Autowired
-    public Parrot(Person person){
-        this.person = person;
-    }
 
     public String getName() {
         return name;
