df4a7e5fbc03978f7ce7656f517503110b6de171
Vasiliy <Mst.Vasek999@yandex.ru>
2024-01-05 12:54:17 +0300

    sq-ch3-ex4 внедрение бинов с помощью аннотации @autowired 1-Внедрение значений через поля класса

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -3,22 +3,11 @@ package config;
 import main.Parrot;
 import main.Person;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan (basePackages = "main")
 public class ProjectConfig {
-    @Bean
-    public Parrot parrot(){
-        Parrot p = new Parrot();
-        p.setName("Koko");
-        return p;
-    }
 
-    @Bean
-    public Person person(Parrot parrot){
-        Person p = new Person();
-        p.setName("Ella");
-        p.setParrot(parrot);    // Монтаж бинов путем прямого вызова
-        return p;
-    }
 }
--- src/main/java/main/Parrot.java
+++ src/main/java/main/Parrot.java
@@ -1,8 +1,11 @@
 package main;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Parrot {
 
-    private String name;
+    private String name = "Koko";
 
     public Parrot(){
         System.out.println("Parrot created!");
