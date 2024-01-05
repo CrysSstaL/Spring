149d311c7d2349f153c1e3ba26085dd9099a2762
Vasiliy <Mst.Vasek999@yandex.ru>
2024-01-05 13:26:06 +0300

    sq-ch3-ex7 Циклическая зависимость (Для создания А надо В, для В надо А)

--- src/main/java/main/Parrot.java
+++ src/main/java/main/Parrot.java
@@ -1,14 +1,17 @@
 package main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Parrot {
 
     private String name = "Koko";
+    private final Person person;
 
-    public Parrot(){
-        System.out.println("Parrot created!");
+    @Autowired
+    public Parrot(Person person){
+        this.person = person;
     }
 
     public String getName() {
--- src/main/java/main/Person.java
+++ src/main/java/main/Person.java
@@ -6,7 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class Person {
     private String name = "Ella";
-    private Parrot parrot;
+    private final Parrot parrot;
+
+    @Autowired
+    public Person(Parrot parrot){
+        this.parrot = parrot;
+    }
 
     public String getName(){
         return name;
@@ -18,12 +23,6 @@ public class Person {
 
     public Parrot getParrot(){
         return parrot;
-    }
-
-    // через сеттер
-    @Autowired
-    public void setParrot(Parrot parrot) {
-        this.parrot = parrot;
     }
 
 }
