288ed1a9de43d429e9c8dc3d537c6549b477bdda
Vasiliy <Mst.Vasek999@yandex.ru>
2024-01-17 19:09:06 +0200

    sq-ch5-ex4 «Ленивое» создание экземпляров с помощью @Lazy

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"main.chapter_four"})
+@ComponentScan(basePackages = {"main.chapter_four.services"})
 public class ProjectConfig {
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,19 +4,15 @@ import config.ProjectConfig;
 import main.chapter_four.model.Comment;
 import main.chapter_four.proxies.EmailCommentNotificationProxy;
 import main.chapter_four.services.CommentService;
-import main.chapter_four.services.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class Main {
     public static void main(String[] args) {
-        var c = new AnnotationConfigApplicationContext(
-                ProjectConfig.class);
-        var s1 = c.getBean(CommentService.class);
-        var s2 = c.getBean(UserService.class);
-        boolean b =
-                s1.getCommentRepository() == s2.getCommentRepository();
-        System.out.println(b);
+        var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        System.out.println("Before retrieving the CommentService");
+        var service = c.getBean(CommentService.class);
+        System.out.println("After retrieving the CommentService");
     }
 }
 
