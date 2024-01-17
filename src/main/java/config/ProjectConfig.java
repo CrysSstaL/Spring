d069e1473412dbc88f761e488dea6765ebf297ff
Vasiliy <Mst.Vasek999@yandex.ru>
2024-01-17 19:25:46 +0200

    sq-ch5-ex5 Объявление бинов с прототипной областью видимости с помощью аннотации @Bean

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -2,11 +2,18 @@ package config;
 
 import main.chapter_four.services.CommentService;
 import main.chapter_one_three.Parrot;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
-@ComponentScan(basePackages = {"main.chapter_four.services"})
 public class ProjectConfig {
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_PROTOTYPE)
+    public CommentService commentService(){
+        return new CommentService();
+    }
 }
--- src/main/java/main/chapter_four/services/CommentService.java
+++ src/main/java/main/chapter_four/services/CommentService.java
@@ -9,10 +9,5 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
-@Lazy
 public class CommentService {
-    public CommentService() {
-        System.out.println("CommentService instance created!");
-    }
 }
