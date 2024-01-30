ffa7122d538cec01ad1afba98150ed0273a677ee
Vasiliy <Mst.Vasek999@yandex.ru>
2024-01-30 13:58:09 +0200

    sq-ch6-ex7 ЦЕПОЧКИ ВЫПОЛНЕНИЯ АСПЕКТОВ(@Order(x)) SecurityAspect-LoggingAspect-publishComment-LoggingAspect-SecurityAspect

--- src/main/java/main/chapter_six/aspects/LoggingAspect.java
+++ src/main/java/main/chapter_six/aspects/LoggingAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ import java.util.logging.Logger;
 
 @Aspect
 @Component
+@Order(2)
 public class LoggingAspect {
     private Logger logger =
             Logger.getLogger(LoggingAspect.class.getName());
