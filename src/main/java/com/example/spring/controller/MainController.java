58eebff01c7f227ed6b53e037294fd6274b4a611
Vasiliy <Mst.Vasek999@yandex.ru>
2024-02-01 21:25:21 +0200

    sq-ch9-ex3 Использование области видимости в рамках веб-приложения

--- src/main/java/com/example/spring/controller/MainController.java
+++ src/main/java/com/example/spring/controller/MainController.java
@@ -1,17 +1,38 @@
 package com.example.spring.controller;
 
+import com.example.spring.service.LoggedUserManagementService;
+import com.example.spring.service.LoginCountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @RequestMapping("/home/{color}")
-    public String home(@PathVariable String color, Model page){
-        page.addAttribute("username", "Katy");
-        page.addAttribute("color", color);
-        return "home.html";
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
+    public MainController(
+            LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+    }
+
+    @GetMapping("/main")
+    public String home(
+            @RequestParam(required = false) String Logout,
+            Model model
+    ) {
+        if (Logout != null) {
+            loggedUserManagementService.setUsername(null);
+        }
+        String username = loggedUserManagementService.getUsername();
+        int count = loginCountService.getCount();
+        if (username == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("username" , username);
+        model.addAttribute("count", count);
+        return "main.html";
     }
 }
--- src/main/java/com/example/spring/model/LoginProcessor.java
+++ src/main/java/com/example/spring/model/LoginProcessor.java
@@ -1,6 +1,7 @@
 package com.example.spring.model;
 
 import com.example.spring.service.LoggedUserManagementService;
+import com.example.spring.service.LoginCountService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -8,16 +9,20 @@ import org.springframework.web.context.annotation.RequestScope;
 @RequestScope
 public class LoginProcessor {
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
     private String username;
     private String password;
 
-    public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService,
+                          LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     public boolean login() {
         String username = this.getUsername();
         String password = this.getPassword();
+        loginCountService.increment();
 
         boolean loginResult = false;
         if ("natalie".equals(username) && "password".equals(password)) {
