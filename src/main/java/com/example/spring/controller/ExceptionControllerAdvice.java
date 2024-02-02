0365b26de91d1067f0e9dd73ddc8b5f85115ce96
Vasiliy <Mst.Vasek999@yandex.ru>
2024-02-02 11:33:25 +0200

    sq-ch10-ex6 Для выполнения логики, созданной на случай, если действие контроллера выдаст исключение, мы добавили в контроллер совет ExceptionControllerAdvice

---
+++ src/main/java/com/example/spring/controller/ExceptionControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.example.spring.controller;
+
+import com.example.spring.details.ErrorDetails;
+import com.example.spring.exception.NotEnoughMoneyException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+    @ExceptionHandler(NotEnoughMoneyException.class)
+    public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setMessage("Not enough money to make the payment.");
+        return ResponseEntity
+                .badRequest()
+                .body(errorDetails);
+    }
+}
